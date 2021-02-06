a92b87dfa9534a8d6c7c3806546f0b99aa2258dd
MADFutcher <mikalob247@gmail.com>
2021-02-06 11:41:30 +0100

    Updated routes for the API GET POST PUT DELETE

--- routes/project-routes.js
+++ routes/project-routes.js
@@ -7,7 +7,78 @@ const Task = require('../models/task-model');
 
 
 router.get('/projects', (req,res,next)=>{
-  res.send('This is working')
+  Project.find()
+         .populate('tasks')
+         .then(response =>{
+           res.json(response)
+         })
+         .catch(err =>{
+           res.status(500).json(err)
+         })
 })
+
+
+//Retrieve Details of Specific Project
+router.get('/projects/:projectId', (req, res, next) =>{
+  if(!mongoose.Types.ObjectId.isValid(req.params.projectId)){
+    res.status(400).json({message: "This is not a valid Project ID, please try again"})
+  }
+
+  Project.findById(req.params.projectId)
+         .then(project =>{
+           res.json(project)
+         })
+         .catch(err=>{
+           res.status(500).json(err)
+         })
+})
+
+router.post('/projects',(req,res,next)=>{
+  const {title, description, tasks} = req.body
+
+  Project.create({
+    title,
+    description,
+    tasks: []
+  })
+  .then(response =>{
+    res.json(response)
+  })
+  .catch(error=>{
+    res.status(500).json(error)
+  })
+
+})
+
+//Update Project
+router.put('/projects/:projectId', (req, res, next) =>{
+  if(!mongoose.Types.ObjectId.isValid(req.params.projectId)){
+    res.status(400).json({message: "This is not a valid Project ID, please try again"})
+  }
+
+  Project.findByIdAndUpdate(req.params.projectId, req.body, { new: true })
+         .then(project =>{
+           res.json({project, message:'Project was successfuly Updated!'} )
+         })
+         .catch(error =>{
+           res.status(500).json(error)
+         })
+})
+
+//Delete Specific Project
+router.delete('/projects/:id', (req, res, next) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    res.status(400).json({ message: 'Specified id is not valid' });
+    return;
+  }
+ 
+  Project.findByIdAndRemove(req.params.id)
+    .then(() => {
+      res.json({ message: `Project with ${req.params.id} is removed successfully.` });
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
 
 module.exports = router
